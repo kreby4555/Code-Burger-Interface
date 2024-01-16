0b7df4274444337321c4582809f4c4dcb0cd5578
kreby4555 <contatocleber822@gmail.com>
2024-01-16 07:09:50 -0300

    tela de login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
+import LoginImg from '../../assets/Hamburger-Login.svg'
+import Logo from '../../assets/Logo.svg'
+import {
+  Container,
+  LoginImage,
+  ContainerItens,
+  Label,
+  Input,
+  Button,
+  SignInLink
+} from './styles'
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LoginImage src={LoginImg} alt="login-image" />
+      <ContainerItens>
+        <img src={Logo} alt="login-logo" />
+        <h1>Login</h1>
+        <Label>Email</Label>
+        <Input />
+
+        <Label>Senha</Label>
+        <Input />
+
+        <Button>Sign In</Button>
+        <SignInLink>
+          Não possui conta ? <a>Sing Up</a>
+        </SignInLink>
+      </ContainerItens>
+    </Container>
   )
 }
 
