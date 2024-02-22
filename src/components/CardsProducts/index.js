de89351e6bfe2d1d60f3e1d1e6e188495a4ec661
kreby4555 <contatocleber822@gmail.com>
2024-02-22 07:37:21 -0300

    Criação da lógica do carrinho!

--- src/components/CardsProducts/index.js
+++ src/components/CardsProducts/index.js
@@ -3,15 +3,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Button } from '../../components'
+import { useCart } from '../../hooks/CartContext'
 import { Container, Image, ParagrafoName, ParagrafoPrice } from './styles'
 export function CardProducts({ product }) {
+  const { putProductInCart } = useCart()
   return (
     <Container>
       <Image src={product.url} alt="Imagem do produto" />
       <div>
         <ParagrafoName>{product.name}</ParagrafoName>
         <ParagrafoPrice>{product.formatedPrice}</ParagrafoPrice>
-        <Button>Adicionar</Button>
+        <Button onClick={() => putProductInCart(product)}>Adicionar</Button>
       </div>
     </Container>
   )
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
+import { CartProvider } from './CartContext'
 import { UserProvider } from './UserContext'
 
-const AppProvider = ({ children }) => <UserProvider>{children}</UserProvider>
+const AppProvider = ({ children }) => (
+  <UserProvider>
+    <CartProvider>{children}</CartProvider>
+  </UserProvider>
+)
 
 AppProvider.propTypes = {
   children: PropTypes.node
