bbe3a37abb3d1960f761ce3c3be92371f17c7116
kreby4555 <contatocleber822@gmail.com>
2024-02-28 08:20:01 -0300

    Estilização do carrino, criação dos botões de adicionar e decrementar, criação da lógica dentro do hook CartContex.

--- src/components/CartItens/styles.js
+++ src/components/CartItens/styles.js
@@ -1,7 +1,60 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background-color: #ffffff;
+  box-shadow: 0px 10px 40px rgba(0, 0, 0.03);
+  border-radius: 20px;
+  padding: 10px;
+  width: max-content;
+`
 
-export const Header = styled.div``
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding: 15px 9px;
+  border-bottom: 1px solid #b5b5b5;
+  p {
+    font-size: 16px;
+    color: #b5b5b5;
+  }
+`
 
-export const Body = styled.div``
+export const Body = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  padding: 10px;
+  grid-gap: 10px 15px;
+
+  img {
+    border-radius: 10px;
+    width: 110px;
+  }
+
+  p {
+    font-size: 16px;
+    color: #000000;
+  }
+
+  .quantity-container {
+    display: flex;
+    gap: 20px;
+
+    button {
+      height: 30px;
+      background: transparent;
+      border: none;
+      font-size: 24px;
+      cursor: pointer;
+    }
+
+    p {
+      margin-top: 5px;
+    }
+  }
+`
+
+export const EmptyCart = styled.p`
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+`
