8994f3a9c617a7e717bdd25c3c337c0ac2fb8885
kreby4555 <contatocleber822@gmail.com>
2024-03-07 08:09:06 -0300

    Criação do FormatDate, e arrumando a tebela

--- src/containers/Admin/Orders/styles.js
+++ src/containers/Admin/Orders/styles.js
@@ -3,4 +3,10 @@ import styled from 'styled-components'
 export const Container = styled.div`
   background-color: #efefef;
   min-height: 100vh;
+  padding: 20px;
+`
+
+export const ProductsImg = styled.img`
+  width: 60px;
+  border-radius: 5px;
 `
