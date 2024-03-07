1ef8382be8a81ae2fb9959247a1714364310ec58
kreby4555 <contatocleber822@gmail.com>
2024-03-07 08:59:12 -0300

    Atualizando os stautus do pedido.

---
+++ src/containers/Admin/Orders/order-status.js
@@ -0,0 +1,33 @@
+const status = [
+  {
+    id: 1,
+    label: 'Pedido realizado',
+    value: 'Pedido realizado'
+  },
+
+  {
+    id: 2,
+    label: 'Preparando',
+    value: 'Preparando'
+  },
+
+  {
+    id: 3,
+    label: 'Pedido pronto',
+    value: 'Pedido pronto'
+  },
+
+  {
+    id: 4,
+    label: 'Pedido a caminho',
+    value: 'Pedido a caminho'
+  },
+
+  {
+    id: 5,
+    label: 'Pedido entregue',
+    value: 'Pedido entregue'
+  }
+]
+
+export default status
