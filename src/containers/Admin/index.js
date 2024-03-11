6088e5a391f9285fd2748078382d89e39a66d20f
kreby4555 <contatocleber822@gmail.com>
2024-03-11 20:49:05 -0300

    Continuando a tela de listagem

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -3,13 +3,15 @@ import React from 'react'
 import { SideMenuadmin } from '../../components'
 import ListProducts from './ListProducts'
 import Orders from './Orders'
-import { Container } from './styles'
+import { Container, ContainerItems } from './styles'
 export function Admin() {
   return (
     <Container>
       <SideMenuadmin />
-      {/* <Orders /> */}
-      <ListProducts />
+      <ContainerItems>
+        {/* <Orders /> */}
+        <ListProducts />
+      </ContainerItems>
     </Container>
   )
 }
