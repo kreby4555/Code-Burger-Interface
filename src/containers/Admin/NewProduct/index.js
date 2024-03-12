87a9063ee9412f5e4751445bf50fe0a4c14e53f3
kreby4555 <contatocleber822@gmail.com>
2024-03-12 08:53:04 -0300

    Criando o formulári do menu edição

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
+import { Button } from '../../../components/Button'
 import api from '../../../services/api'
-import { Container } from './styles'
+import { Container, Label, Input } from './styles'
+
 function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get('products')
@@ -13,7 +19,20 @@ function NewProduct() {
 
   return (
     <Container>
-      <div>Ok</div>
+      <form noValidate>
+        <Label>Nome</Label>
+        <Input type="text" {...register('name')} />
+
+        <Label>Preço</Label>
+        <Input type="number" {...register('price')} />
+
+        <Label>Upload</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+
+        <Button>Adicionar Produtos</Button>
+      </form>
     </Container>
   )
 }
