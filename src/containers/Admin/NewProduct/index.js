3493d2813eab0ac73df51986c9254bc3e66ff185
kreby4555 <contatocleber822@gmail.com>
2024-03-14 08:43:39 -0300

    Criando o input de carregar as imagens

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import ReactSelect from 'react-select'
 
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload'
+
 import api from '../../../services/api'
-import { Container, Label, Input, ButtonStyles } from './styles'
+import { Container, Label, Input, ButtonStyles, LabelUpload } from './styles'
 
 function NewProduct() {
+  const [filename, setFileName] = useState(null)
   const { register, handleSubmit } = useForm()
   const onSubmit = data => console.log(data)
   useEffect(() => {
@@ -25,8 +28,23 @@ function NewProduct() {
         <Label>Preço</Label>
         <Input type="number" {...register('price')} />
 
-        <Label>Upload</Label>
-        <Input type="file" accept="image/png, image/jpeg" />
+        <LabelUpload>
+          {filename || (
+            <>
+              <DriveFolderUploadIcon />
+              Carregue a imagem do produto
+            </>
+          )}
+
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            {...register('file')}
+            onChange={value => {
+              setFileName(value.target.files[0]?.name)
+            }}
+          />
+        </LabelUpload>
 
         <ReactSelect />
 
